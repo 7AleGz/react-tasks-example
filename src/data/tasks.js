0c1f43ef7ed62ee61b2fb63614c6ae5286d82e5e
7AleGz <7alegz@gmail.com>
2023-07-08 15:32:53 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "my first task",
+        description: "description of my first task"
+    },
+    {
+        id: 1,
+        title: "my second task",
+        description: "description of my second task"
+    },
+    {
+        id: 2,
+        title: "my third task",
+        description: "description of my third task"
+    },
+];
